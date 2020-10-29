21460c0117f0ef36188d34f84a5de7ee0b836896
iosdev <mohammhu@metropolia.fi>
2020-10-29 22:13:03 +0200

    the add function should work now

--- Swift lab 2.playground/Contents.swift
+++ Swift lab 2.playground/Contents.swift
@@ -2,61 +2,73 @@ import UIKit
 
 
 
-class Book{
+
+class Book: Hashable{
     let name: String
     let purchaseID: Int
     let marketPrice: Double
-    
-    
+
+
     init(name: String, purchaseID: Int, marketPrice: Double) {
         self.name = name
         self.purchaseID = purchaseID
         self.marketPrice = marketPrice
     }
-    
-    
+    static func == (lhs: Book, rhs: Book) -> Bool {
+         return lhs.purchaseID == rhs.purchaseID
+     }
+     
+     func hash(into hasher: inout Hasher) {
+         hasher.combine(purchaseID)
+     }
+
     func bookPrinter() -> String {
         return "Name: \(self.name) Price: \(marketPrice) Purchace Id: \(purchaseID)"
     }
-        
+
 }
 
 class Shelf{
-    private let shelfName: String
-    private var books = [Book]()
-    
-    init(shelfName: String) {
-        self.shelfName = shelfName
-    }
+      private let shelfName: String
+     private var setOfBooks = Set<Book>()
 
-    func add(book : Book ){
-        self.books.append(book)
-    }
-    
-    func delete(book : Book){
-    
-    }
-    
+     init(shelfName: String) {
+         self.shelfName = shelfName
+     }
+
+     func add(book: Book){
+         if !setOfBooks.contains(book) {
+             setOfBooks.insert(book)
+         }
+     }
+
+     func delete(book: Book){
+         setOfBooks.remove(book)
+     }
+   
+
     
     func printer() {
-        for b in self.books{
-            print(b.bookPrinter())
+        for books in self.setOfBooks{
+            print(books.bookPrinter())
         }
+            
+
     }
-    
-  
-    
 }
 
 
-var book = Book(name: "A book", purchaseID: 12, marketPrice: 9.90)
-
-var bookIntheshelf = Shelf(shelfName: "Book Shelf")
 
 
-bookIntheshelf.add(book: book)
-bookIntheshelf.add(book: book)
-bookIntheshelf.add(book: book)
+var myBooks = Book(name: "my book", purchaseID: 12, marketPrice: 12.9)
+var myBook = Book(name: "my book", purchaseID: 13, marketPrice: 12.9)
 
 
-bookIntheshelf.printer()
+var myShelf = Shelf(shelfName: "My Shalf")
+
+
+myShelf.add(book: myBooks)
+myShelf.add(book: myBook)
+
+
+myShelf.printer()
